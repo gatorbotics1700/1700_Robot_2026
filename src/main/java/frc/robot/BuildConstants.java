b98d002b8bf630a0fd5fe1ad395accb0203d9b4c
Anne Kelley <annekelley@Annes-MacBook-Air.local>
2026-01-22 15:35:47 -0800

    AUTO path creation ak

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "1700_Robot_2026";
+  public static final String MAVEN_NAME = "1700_Robot_2026-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "79c3c2903bb7ce5d08820902d3bbd4f60d083a05";
-  public static final String GIT_DATE = "2026-01-17 13:44:52 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-17 15:02:19 EST";
-  public static final long BUILD_UNIX_TIME = 1768680139953L;
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "4d67446e3cba2869e9c7628d28ea1bc71f460ffe";
+  public static final String GIT_DATE = "2026-01-17 15:27:10 EST";
+  public static final String GIT_BRANCH = "autopaths_annekel";
+  public static final String BUILD_DATE = "2026-01-21 18:39:17 EST";
+  public static final long BUILD_UNIX_TIME = 1769038757684L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
