a4fc232f380bdf768bf8a87a0bf72d104f24558c
gatorbotics <gatorbotics1700@gmail.com>
2026-01-27 19:54:36 -0800

    added camera offsets

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "1700_Robot_2026-Imported";
+  public static final String MAVEN_NAME = "1700_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "9f391fa99e4cde716c3e34928adfb71b9e93e04f";
-  public static final String GIT_DATE = "2026-01-22 19:49:31 PST";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "f05993584cf399c64986a883b94d4e493e1e8e50";
+  public static final String GIT_DATE = "2026-01-23 17:12:48 PST";
   public static final String GIT_BRANCH = "multicameras";
-  public static final String BUILD_DATE = "2026-01-23 17:12:41 PST";
-  public static final long BUILD_UNIX_TIME = 1769217161383L;
+  public static final String BUILD_DATE = "2026-01-27 19:09:29 PST";
+  public static final long BUILD_UNIX_TIME = 1769569769458L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
