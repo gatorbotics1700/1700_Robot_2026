5b39d1808e9436354e443eed5da0e93dd014e738
Violet Chaffee <26vchaffee@castilleja.org>
2026-02-01 17:23:40 -0800

    got rid of things that duplicated in the merge

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "1700_Robot_2026";
+  public static final String MAVEN_NAME = "1700_Robot_2026-Imported";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 40;
-  public static final String GIT_SHA = "c642319e18f1c743b8f55441b91430b423dbea46";
-  public static final String GIT_DATE = "2026-02-01 17:12:01 PST";
-  public static final String GIT_BRANCH = "autopaths_annekel";
-  public static final String BUILD_DATE = "2026-02-01 17:12:35 PST";
-  public static final long BUILD_UNIX_TIME = 1769994755404L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 46;
+  public static final String GIT_SHA = "e24b3952da0b0e6bbbb5187f0aca7041549a84ce";
+  public static final String GIT_DATE = "2026-02-01 17:17:35 PST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-01 17:23:17 PST";
+  public static final long BUILD_UNIX_TIME = 1769995397997L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
