790ed4899ed7dcdbae6cb9b1fa703c6974163a2b
AmeliaY07 <ameliayuan07@gmail.com>
2026-02-03 18:48:50 -0800

    added shooter IO, probs don't need

---
+++ src/main/java/frc/robot/subsystems/mech/ShooterIO.java
@@ -0,0 +1,30 @@
+// Copyright 2021-2025 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot.subsystems.mech;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ShooterIO {
+  @AutoLog
+  public static class ShooterIOInputs {
+    /** Motor velocity in revolutions per second. */
+    public double flywheelVelocityRevsPerSec = 0.0;
+    public double transitionVelocityRevsPerSec = 0.0;
+  }
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(ShooterIOInputs inputs) {}
+
+  /** Run open loop at the specified speed (duty cycle, typically -1 to 1). */
+  public default void setSpeed(double speed) {}
+}
---
+++ src/main/java/frc/robot/subsystems/mech/ShooterIOSim.java
@@ -0,0 +1,42 @@
+// Copyright 2021-2025 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot.subsystems.mech;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.subsystems.mech.ShooterIO.ShooterIOInputs;
+
+/** Simulated hood IO: integrates speed to get position in revs. */
+public class ShooterIOSim implements ShooterIO {
+
+  private double positionRevs = 0.0;
+  private double flywheelSpeed = 0.0;
+  private double transitionSpeed = 0.0;
+  private double lastTime = Timer.getFPGATimestamp();
+
+  @Override
+  public void updateInputs(ShooterIOInputs inputs) {
+    double now = Timer.getFPGATimestamp();
+    double dt = now - lastTime;
+    lastTime = now;
+
+    inputs.flywheelVelocityRevsPerSec = flywheelSpeed;
+    inputs.transitionVelocityRevsPerSec = transitionSpeed;
+  }
+
+  @Override
+  public void setSpeed(double speed) {
+    this.flywheelSpeed = speed;
+    this.transitionSpeed = speed;
+  }
+}
