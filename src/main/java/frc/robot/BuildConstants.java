e88de57bc10be7762bb110d849766845b45d0316
gatorbotics <gatorbotics1700@gmail.com>
2026-02-03 19:58:32 -0800

    began testing motion magic - need to fix some overshooting error and maybe gear ratio?

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -6,12 +6,12 @@ public final class BuildConstants {
   public static final String MAVEN_NAME = "1700_Robot_2026";
   public static final String MAVEN_NAME = "1700_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 123;
-  public static final String GIT_SHA = "e82f9ffbb6ee0d02ba52cb49ce51367a863a8dfc";
-  public static final String GIT_DATE = "2026-02-06 11:27:22 PST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-06 11:40:24 PST";
-  public static final long BUILD_UNIX_TIME = 1770406824842L;
+  public static final int GIT_REVISION = 90;
+  public static final String GIT_SHA = "062c889eda049d6849aae07a4f0bf684dc2870bc";
+  public static final String GIT_DATE = "2026-02-03 17:33:28 PST";
+  public static final String GIT_BRANCH = "mech_motionmagic";
+  public static final String BUILD_DATE = "2026-02-03 19:52:54 PST";
+  public static final long BUILD_UNIX_TIME = 1770177174703L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
