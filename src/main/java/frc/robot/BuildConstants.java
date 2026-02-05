c5c577c070022a26b98bd5ef4a775e6ed4495eba
gatorbotics <gatorbotics1700@gmail.com>
2026-02-04 20:10:01 -0800

    *WARNING* Do not turn on the robot unless you want to hear endless beeping... added tuneable constants but issues b/c we have to keep reconfiguring the motor (hence the beeping) so this needs to be fixed!

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "1700_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 92;
-  public static final String GIT_SHA = "bd08f7242e448ff49749ae818a009fa5243226f4";
-  public static final String GIT_DATE = "2026-02-03 19:58:59 PST";
+  public static final int GIT_REVISION = 93;
+  public static final String GIT_SHA = "113c5d2e52df35327a71acb58bb3302193f6a775";
+  public static final String GIT_DATE = "2026-02-04 17:10:00 PST";
   public static final String GIT_BRANCH = "mech_motionmagic";
-  public static final String BUILD_DATE = "2026-02-04 17:08:05 PST";
-  public static final long BUILD_UNIX_TIME = 1770253685326L;
+  public static final String BUILD_DATE = "2026-02-04 20:04:37 PST";
+  public static final long BUILD_UNIX_TIME = 1770264277795L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
