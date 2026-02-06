e82f9ffbb6ee0d02ba52cb49ce51367a863a8dfc
Violet Chaffee <26vchaffee@castilleja.org>
2026-02-06 11:27:22 -0800

    got rid of setneutralmode periodically

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "1700_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 121;
-  public static final String GIT_SHA = "3946552634781251f30a3023e127ac3ae2e8da25";
-  public static final String GIT_DATE = "2026-02-05 10:54:47 PST";
+  public static final int GIT_REVISION = 122;
+  public static final String GIT_SHA = "0305c2620840c736030ade6190ec34768459e151";
+  public static final String GIT_DATE = "2026-02-05 11:13:40 PST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-05 11:12:40 PST";
-  public static final long BUILD_UNIX_TIME = 1770318760407L;
+  public static final String BUILD_DATE = "2026-02-06 11:26:18 PST";
+  public static final long BUILD_UNIX_TIME = 1770405978582L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
