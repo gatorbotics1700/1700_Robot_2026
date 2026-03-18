1fdd52ef06723eaa595cfb584ad602ef51ebdd43
vivienguo <27vguo@castilleja.org>
2026-03-17 17:26:41 -0700

    created drivetrain system check button

---
+++ src/main/java/frc/robot/commands/drive/DriveSystemsCheckCommands.java
@@ -0,0 +1,52 @@
+package frc.robot.commands.drive;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.drive.Drive;
+
+public class DriveSystemsCheckCommands {
+  private DriveSystemsCheckCommands() {}
+
+  public static class DriveForThreeSeconds extends Command {
+    // include any subsystem requirements here
+    private final Drive drive;
+    private double startTimeMillis;
+    private ChassisSpeeds chassisSpeeds;
+
+    // constructor
+    public DriveForThreeSeconds(Drive drive, ChassisSpeeds chassisSpeeds) {
+      this.drive = drive;
+      this.chassisSpeeds = chassisSpeeds;
+      addRequirements(drive);
+    }
+
+    @Override
+    public void initialize() {
+      startTimeMillis = System.currentTimeMillis();
+    }
+
+    @Override
+    public void execute() {
+      drive.runVelocity(chassisSpeeds);
+    }
+
+    @Override
+    public boolean isFinished() {
+      return System.currentTimeMillis() - startTimeMillis > 3000;
+    }
+  }
+
+  public static Command DriveSystemCheckCommand(Drive drive) {
+    return new DriveForThreeSeconds(
+            drive, ChassisSpeeds.fromFieldRelativeSpeeds(0.8, 0, 0, drive.getRotation()))
+        .andThen(new WaitCommand(1))
+        .andThen(
+            new DriveForThreeSeconds(
+                drive, ChassisSpeeds.fromFieldRelativeSpeeds(0, 0.8, 0, drive.getRotation())))
+        .andThen(new WaitCommand(1))
+        .andThen(
+            new DriveForThreeSeconds(
+                drive, ChassisSpeeds.fromFieldRelativeSpeeds(0, 0, 1, drive.getRotation())));
+  }
+}
